879ff69613362cc5637614f926a7df4638d2fab4
maskd_megane <maskd.megane@gmail.com>
2011-04-11 21:41:38 +0900

    kaminari導入

--- Gemfile
+++ Gemfile
@@ -29,3 +29,5 @@ gem 'sqlite3-ruby', '=1.3.2', :require => 'sqlite3'
 # group :development, :test do
 #   gem 'webrat'
 # end
+
+gem 'kaminari'
--- test/functional/events_controller_test.rb
+++ test/functional/events_controller_test.rb
@@ -11,6 +11,24 @@ class EventsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:events)
   end
 
+  test "should get index for pagination" do
+    25.times do |i|
+      Event.create(:name => "event_#{i}")
+    end
+    get :index
+    assert_response :success
+    assert_equal assigns(:events).size, 25
+
+    get :index, :page => 1
+    assert_equal assigns(:events).size, 25
+
+    get :index, :page => 2
+    assert_equal assigns(:events).size, 4
+
+    get :index, :page => 3
+    assert_blank assigns(:events)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
